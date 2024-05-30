15561080002557d645e37947fcc496f2558db507
manish <manishkumar60708090@gmail.com>
2024-05-30 14:51:10 +0530

    fundatmental structure of react-table

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import {useTable} from "react-table"
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const data = [
+
+  {
+    id:1,
+    gender:"Male",
+    salary:40000
+  },
+  {
+    id:1,
+    gender:"Male",
+    salary:40000
+  },
+  {
+    id:1,
+    gender:"Male",
+    salary:40000
+  },
+  {
+    id:1,
+    gender:"Male",
+    salary:40000
+  },
+  {
+    id:1,
+    gender:"Male",
+    salary:40000
+  },
+  {
+    id:1,
+    gender:"Male",
+    salary:40000
+  },
+
+]
+
+const columns = [
+
+
+  // what is the difference between Header and accessor, Header is see to in the UI, accessor means id 
+  // if you want to Header then use the accessor 
+  {
+    Header:"Id",
+    accessor:"id"
+  },
+  {
+    Header:"Gender",
+    accessor:"gender"
+  },
+  {
+    Header:"Salary",
+    accessor:"salary"
+  },
+
+
+
+]
+
+const App = () => {
+
+  const {
+    getTableProps,
+    getTableBodyProps, 
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns,
+    data
+  })
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='ccontainer'>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Gender</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((i) => (
+              <tr key={i.id}>
+                <td>{i.id}</td>
+                <td>{i.gender}</td>
+                <td>{i.salary}</td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+
+    </div>
   )
 }
 
